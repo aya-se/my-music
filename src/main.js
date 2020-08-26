ca2a8990c42d851e6d04cbcec0d0421e0932993f
aya-se <kakkeno1@gmail.com>
2020-08-27 04:18:59 +0900

    サイト仮完成

--- src/main.js
+++ src/main.js
@@ -2,10 +2,14 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import "./plugins/element.js";
+import vuetify from "./plugins/vuetify";
+import "vuetify/dist/vuetify.min.css";
 
+Vue.use(vuetify);
 Vue.config.productionTip = false;
 
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount("#app");
--- src/views/Music.vue
+++ src/views/Music.vue
@@ -4,11 +4,11 @@
       <h1><i class="el-icon-search" /> Search</h1>
       <p>
         タグや曲名から、表示する音楽を絞り込むことが出来ます！
-        <el-switch v-model="isValid" active-text="表示" inactive-text="非表示">
+        <el-switch v-model="isSearch" active-text="表示" inactive-text="非表示">
         </el-switch>
       </p>
       <el-collapse-transition>
-        <div v-if="isValid">
+        <div v-if="isSearch">
           <el-alert type="success" show-icon :closable="false"
             ><b>検索タグ</b
             >にタグを登録すると、そのタグが登録されている音楽のみが表示されます。</el-alert
@@ -17,7 +17,7 @@
             ><b>除外タグ</b
             >にタグを登録すると、そのタグが登録されている音楽が全て非表示になります。</el-alert
           >
-          <el-card class="box-card"
+          <el-card class="box-card" shadow="hover"
             ><el-alert type="default" show-icon :closable="false"
               ><b>タグ一覧</b>
               ：タグをクリックするとタグの属性が変化します(→未登録→検索タグ→除外タグ→の順)</el-alert
@@ -27,13 +27,15 @@
                 v-if="t.state === 0"
                 class="tag"
                 plain
+                size="mini"
                 type="default"
                 @click="onClickTag(t.id, 1)"
-                >{{ t.name }}</el-button
+                >{{ t.id }}{{ t.name }}</el-button
               >
               <el-button
                 v-if="t.state === 1"
                 class="tag"
+                size="mini"
                 type="success"
                 @click="onClickTag(t.id, 2)"
                 >{{ t.name }}</el-button
@@ -41,13 +43,12 @@
               <el-button
                 v-if="t.state === 2"
                 class="tag"
+                size="mini"
                 type="danger"
                 @click="onClickTag(t.id, 0)"
                 >{{ t.name }}</el-button
               >
             </div>
-          </el-card>
-          <el-card class="box-card">
             <el-alert type="default" show-icon :closable="false"
               ><b>曲名検索</b
               >：入力した曲名が部分一致するような曲のみを表示します
@@ -59,13 +60,11 @@
                 </el-form-item>
               </el-form>
             </p>
-          </el-card>
-          <el-card class="box-card">
             <el-alert type="default" show-icon :closable="false"
               ><b>並び替え</b>
             </el-alert>
             <p>
-              <el-radio-group v-model="radio">
+              <el-radio-group v-model="radio" @change="onSort(radio)">
                 <el-radio :label="0">新しい順</el-radio>
                 <el-radio :label="1">古い順</el-radio>
                 <el-radio :label="2">曲が長い順</el-radio>
@@ -78,49 +77,191 @@
     </div>
     <div id="music">
       <h1><i class="el-icon-headset" /> Music</h1>
-      <div v-for="m in musics" :key="m.id">
-        <el-card class="box-card">
-          <h2 class="music-title">
-            <i class="el-icon-caret-right" />{{ m.name }}
-          </h2>
-          <i class="el-icon-date" />{{ m.release }} <i class="el-icon-time" />{{
-            m.time
-          }}
-          <p>
-            <el-button
-              icon="el-icon-video-play"
-              circle
-              @click="onPlay(m.id)"
-              v-if="!m.playing"
-            />
-            <el-button
-              icon="el-icon-video-pause"
-              circle
-              @click="onStop(m.id)"
-              v-if="m.playing"
-            />
-            <audio
-              id="player"
-              class="audio"
-              src="../assets/Daylight as usual.mp3"
-              controls
-              preload="none"
-            />
-          </p>
-        </el-card>
+      <div v-for="m in musics" :key="m[0]">
+        <transition name="el-fade-in-linear">
+          <el-card
+            class="box-card"
+            shadow="hover"
+            v-if="m[1].includes(musicName)"
+          >
+            <div class="card-top">
+              <div class="card-content1">
+                <h3 class="music-title">
+                  <i class="el-icon-caret-right" />{{ m[1] }}
+                </h3>
+                <p style="margin-left:10px;">
+                  <i class="el-icon-date" />{{ m[3] }}
+                  <i class="el-icon-time" />{{ m[4] }}
+                </p>
+              </div>
+              <div class="card-content2">
+                <el-button
+                  v-if="!isPlaying || playingMusicName !== m[1]"
+                  type="primary"
+                  icon="el-icon-video-play"
+                  circle
+                  @click="onPlay(m[1], m[2])"
+                />
+                <el-button
+                  v-if="isPlaying && playingMusicName === m[1]"
+                  type="primary"
+                  icon="el-icon-video-pause"
+                  circle
+                  @click="onPause()"
+                />
+                <el-button
+                  v-if="m[5] == 1"
+                  type="primary"
+                  icon="el-icon-download"
+                  circle
+                  @click="onDownload(m[2], m[1])"
+                />
+                <el-button
+                  v-if="m[5] == 0"
+                  type="primary"
+                  icon="el-icon-download"
+                  disabled
+                  circle
+                />
+                <el-button
+                  v-if="m[6].length > 70 && !expander[Number(m[0])]"
+                  type="primary"
+                  icon="el-icon-chat-dot-round"
+                  @click="onExpand(Number(m[0]), true)"
+                  circle
+                />
+                <el-button
+                  v-if="m[6].length > 70 && expander[Number(m[0])]"
+                  type="primary"
+                  icon="el-icon-close"
+                  @click="onExpand(Number(m[0]), false)"
+                  circle
+                />
+                <el-button
+                  v-if="m[6].length <= 70"
+                  type="primary"
+                  icon="el-icon-chat-dot-round"
+                  disabled
+                  circle
+                />
+              </div>
+            </div>
+            <el-alert
+              :closable="false"
+              type="default"
+              style="width:100% !important;padding:0px !important;"
+            >
+              <p
+                class="tag-list"
+                v-for="(t, j) in musicTag[Number(m[0])]"
+                :key="j"
+              >
+                <el-tag
+                  class="tag2"
+                  v-if="t === 1"
+                  type="info"
+                  effect="plain"
+                  size="mini"
+                  >{{ tags[j].name }}</el-tag
+                >
+              </p>
+            </el-alert>
+            <div v-if="m[6].length > 70">
+              <p class="music-text" v-if="!expander[Number(m[0])]">
+                {{ m[6].slice(0, 70) }}...
+              </p>
+              <p class="music-text" v-if="expander[Number(m[0])]">
+                {{ m[6] }}
+              </p>
+            </div>
+            <div v-if="m[6].length <= 70">
+              <p class="music-text">{{ m[6] }}</p>
+            </div>
+          </el-card>
+        </transition>
       </div>
+    </div>
+    <div style="width:100%;height:150px;" />
+    <audio id="audio" src="" preload="auto" loop />
+    <div class="player" v-if="isPlayer">
+      <h3><i class="el-icon-loading" /> {{ playingMusicName }}</h3>
+      <p>
+        <el-slider
+          class="music-player"
+          v-model="musicTimeRate"
+          :show-tooltip="false"
+          @change="onSliderChange()"
+        ></el-slider>
+        <el-button
+          v-if="!isPlaying"
+          class="player-button"
+          type="primary"
+          icon="el-icon-video-play"
+          circle
+          @click="
+            onPlay(playingMusicName, '/music/' + playingMusicName + '.mp3')
+          "
+        />
+        <el-button
+          v-if="isPlaying"
+          class="player-button"
+          type="primary"
+          icon="el-icon-video-pause"
+          circle
+          @click="onPause()"
+        />
+        {{ Math.floor(musicNowTime / 60) }}:{{
+          Math.floor((musicNowTime % 60) / 10)
+        }}{{ Math.floor((musicNowTime % 60) % 10) }}/{{
+          Math.floor(musicLength / 60)
+        }}:{{ Math.floor((musicLength % 60) / 10)
+        }}{{ Math.floor((musicLength % 60) % 10) }}
+      </p>
     </div>
   </div>
 </template>
 
 <script>
+function getCsv(url) {
+  //CSVファイルを文字列で取得。
+  var txt = new XMLHttpRequest();
+  txt.open("get", url, false);
+  txt.send();
+
+  //改行ごとに配列化
+  var arr = txt.responseText.split("\n");
+
+  //1次元配列を2次元配列に変換
+  var res = [];
+  for (var i = 0; i < arr.length; i++) {
+    //空白行が出てきた時点で終了
+    if (arr[i] == "") break;
+
+    //","ごとに配列化
+    res[i] = arr[i].split(",");
+  }
+
+  return res;
+}
 export default {
   name: "music",
+  components: {
+    //VuetifyAudio: () => import("vuetify-audio")
+  },
   data() {
     return {
-      isValid: true,
+      isSearch: true,
+      isPlayer: false,
+      isSliding: false,
+      isPlaying: false,
       radio: 0,
       musicName: "",
+      musicLength: 0,
+      musicNowTime: 0,
+      musicTimeRate: 0,
+      playingMusicName: "",
+      prevTime: 0,
+      expander: [],
       tags: [
         { id: 0, name: "明るい", state: 0 },
         { id: 1, name: "楽しい", state: 0 },
@@ -169,19 +310,46 @@ export default {
         { id: 44, name: "galGame", state: 0 },
         { id: 45, name: "Hackathon", state: 0 }
       ],
-      musics: [
-        {
-          id: "0",
-          name: "八月のRecollection",
-          release: "2019-07-06",
-          time: "4:30",
-          text:
-            "日本の夏の終わり頃、ちょっと涼しい山の森の中、みたいなイメージの曲です。前作「夏に溶けた幻想」がイントロで曲が終わってしまうという残念な結果だったので、そのリベンジも兼ねていて、コンセプトや楽器構成は比較的そのまま受け継いで、メロディは1から作り直したという経緯があります。今までの自作曲の中では一番シリアス(?)だけど決してダークではない、爽やかさ強めな感じになったと思います。最近の都会の夏は猛暑だのゲリラ豪雨だので風流さの欠片もありませんが…(-ω-;)。今までの曲はどうしても無意識に普段聴いている東方原曲っぽいものが随所に付与されてしまっていた(これ自体は別に悪いことじゃない気もするけど)ので、今回は同じ和風でも東方ではない、どちらかと言えばCLANNADとかAIRとかのKey作品って感じの和風を目指しました。そういえば曲の構成は第2弾の自作曲「プレザントラベラー」にそっくりですね。個人的にはそこまで曲をすごく長くはしたくないという気持ちがある(最後まで聴くのが大変になってしまうので)ので、これでも出来るだけ余計なパートは省いてるつもりなんですけど、今回は実質BPM100なので(制作上はBPM200",
-          playing: false,
-          tag: [0, 5, 9, 12]
-        }
-      ]
+      musicTag: [],
+      tagData: [],
+      //id,name,url,release,time,download,textの順
+      musics: []
     };
+  },
+  mounted: function() {
+    this.tagData = getCsv("/data/tagData.csv");
+    this.musics = getCsv("/data/musics.csv");
+    this.onSort(0);
+    this.expander = new Array(this.musics.length).fill(false);
+    this.musicTag = new Array(this.musics.length);
+    for (let i = 0; i < this.musicTag.length; i++) {
+      this.musicTag[i] = new Array(this.tags.length);
+      for (let j = 0; j < this.musicTag[i].length; j++) {
+        this.musicTag[i][j] = 0;
+      }
+    }
+    for (let i = 0; i < this.tagData.length; i++) {
+      this.musicTag[Number(this.tagData[i][0])][Number(this.tagData[i][1])] = 1;
+    }
+    setInterval(() => {
+      if (
+        !isNaN(document.querySelector("#audio").currentTime) &&
+        !isNaN(this.musicTimeRate)
+      ) {
+        this.musicNowTime = document.querySelector("#audio").currentTime;
+      }
+      if (!isNaN(document.querySelector("#audio").duration))
+        this.musicLength = document.querySelector("#audio").duration;
+      if (
+        !isNaN(this.prevTime) &&
+        !isNaN(this.musicTimeRate) &&
+        this.prevTime !== this.musicTimeRate
+      )
+        this.isSliding = true;
+      if (!this.isSliding)
+        this.musicTimeRate = (this.musicNowTime * 100) / this.musicLength;
+      this.prevTime = this.musicTimeRate;
+    }, 100);
   },
   methods: {
     onClick(url) {
@@ -190,17 +358,76 @@ export default {
     onClickTag(id, next) {
       this.$set(this.tags[id], "state", next);
     },
-    onPlay(id) {
-      this.musicSrc();
-      this.musics[id].playing = true;
+    onPlay(name, url) {
+      if (this.playingMusicName !== name) {
+        document.querySelector("#audio").src = url;
+        this.musicTimeRate = 0;
+        this.prevTime = 0;
+        document.querySelector("#audio").currentTime = 0;
+      }
+      this.playingMusicName = name;
+      document.querySelector("#audio").play();
+      this.isSliding = false;
+      this.isPlayer = true;
+      this.isPlaying = true;
     },
-    onStop(id) {
-      this.musics[id].playing = false;
+    onPause() {
+      document.querySelector("#audio").pause();
+      this.isPlaying = false;
+    },
+    onSliderChange() {
+      document.querySelector("#audio").currentTime =
+        (this.musicTimeRate * this.musicLength) / 100;
+      setTimeout(() => {
+        this.isSliding = false;
+      }, 200);
+    },
+    onSort(type) {
+      if (type === 0 || type === 1) {
+        this.musics.sort(function(a, b) {
+          if (a[3] < b[3]) {
+            return -1;
+          } else if (a[3] > b[3]) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+      } else {
+        this.musics.sort(function(a, b) {
+          if (a[4] < b[4]) {
+            return -1;
+          } else if (a[4] > b[4]) {
+            return 1;
+          } else {
+            return 0;
+          }
+        });
+      }
+      if (type === 0 || type === 2) this.musics.reverse();
+      return;
+    },
+    onExpand(id, value) {
+      this.$set(this.expander, id, value);
+    },
+    onDownload(url, name) {
+      let link = document.createElement("a");
+      link.href = url;
+      link.download = name;
+      link.click();
     }
   }
 };
 </script>
 <style scoped>
+.player {
+  position: fixed;
+  bottom: 0px;
+  width: 100%;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  background-color: whitesmoke;
+}
 .home {
   overflow: hidden;
 }
@@ -231,17 +458,75 @@ export default {
 }
 .tag-list {
   display: inline-block !important;
+  float: left;
 }
 .tag {
-  padding-left: 10px !important;
-  padding-right: 10px !important;
-  padding-top: 7.5px !important;
-  padding-bottom: 7.5px !important;
-  margin-left: 5px !important;
-  margin-right: 5px !important;
-  margin-top: 10px !important;
+  padding-left: 5px !important;
+  padding-right: 5px !important;
+  padding-top: 5px !important;
+  padding-bottom: 5px !important;
+  margin-left: 2.5px !important;
+  margin-right: 2.5px !important;
+  margin-top: 5px !important;
+}
+.tag2 {
+  margin-right: 5px;
+  margin-bottom: 5px;
 }
 .music-title {
   color: orangered;
+}
+.music-text {
+  align-items: flex-end;
+  text-align: left;
+  font-size: small;
+  color: rgb(100, 100, 100);
+}
+.music-player {
+  display: inline-block;
+  width: 80%;
+  margin-left: 30px;
+  float: left;
+}
+.player-button {
+  margin: 0 auto;
+  display: inline-block;
+  margin-right: 10px;
+}
+@media screen and (min-width: 600px) {
+  .card-top {
+    height: 50px;
+  }
+  .card-content1 {
+    width: 70%;
+    height: 50px;
+    margin: auto;
+    text-align: left;
+    float: left;
+  }
+  .card-content2 {
+    display: inline-block;
+    width: 30%;
+    height: 50px;
+    float: right;
+  }
+}
+@media screen and (max-width: 600px) {
+  .card-top {
+    height: auto;
+  }
+  .card-content1 {
+    width: 100%;
+    height: auto;
+    margin: auto;
+    text-align: left;
+    float: left;
+  }
+  .card-content2 {
+    width: 100%;
+    height: auto;
+    margin: auto;
+    float: left !important;
+  }
 }
 </style>
